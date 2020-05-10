bb50cc996ceb4e94196792ab3cab2f82092f14b6
MItchell Hillman <mitchellhillman@MItchells-MacBook-Air.local>
2020-05-10 14:16:59 -0400

    Add build

---
+++ build/precache-manifest.99c3e60d304475d2ca9b9ba370157511.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5137c84b52c7a209eab6bac00cccfc60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe59e2cd1108bd24c92",
+    "url": "/static/css/main.0cc1f6e0.chunk.css"
+  },
+  {
+    "revision": "0f5c6d667bb4a8b9b0ad",
+    "url": "/static/js/2.6933a724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6933a724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe59e2cd1108bd24c92",
+    "url": "/static/js/main.a8de6170.chunk.js"
+  },
+  {
+    "revision": "d34ec583db180ee1b51c",
+    "url": "/static/js/runtime-main.74f78e64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
